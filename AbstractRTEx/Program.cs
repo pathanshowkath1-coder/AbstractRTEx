f95dbe1e449b525b550922a3248ea475fe86a2eb
Showkath <pathanshowkath1@gmail.com>
2026-05-28 11:44:18 +0530

    Add project files.

---
+++ AbstractRTEx/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractRTEx
+{
+    abstract class HMWBS
+    {
+        public void BillDetails()
+        {
+            Console.WriteLine(" HMWBS Bill Details");
+        }
+        public abstract void GetBill( double units);
+    }
+    class ResidentialCustomer : HMWBS
+    {
+        public override void GetBill(double units)
+        {
+            double bill = units * 7;
+            Console.WriteLine("Residential Customer Bill is"+ bill);
+
+        }
+    }
+    class CommercialCustomer : HMWBS
+    {
+        public override void GetBill(double units)
+        {
+            double bill = units * 10;
+            Console.WriteLine("Commercial Customer Bill is" + bill);
+
+        }
+    }
+    class AgriculturalCustomer : HMWBS
+    {
+        public override void GetBill(double units)
+        {
+            double bill = units * 5;
+            Console.WriteLine("Agricultural Customer Bill is" + bill);
+        }
+    }
+    class industrialCustomer : HMWBS
+    {
+        public override void GetBill(double units)
+        {
+            double bill = units * 15;
+            Console.WriteLine("Industrial Customer Bill is" + bill);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HMWBS obj = new ResidentialCustomer();
+            obj.BillDetails();
+            Console.WriteLine("Enter the units consumed:");
+            double units = Convert.ToDouble(Console.ReadLine());
+            obj.GetBill(units);
+            obj = new CommercialCustomer();
+            obj.GetBill(units);
+            obj= new AgriculturalCustomer();
+            obj.GetBill(units);
+            obj = new industrialCustomer();
+            obj.GetBill(units);
+
+        }
+    }
+}
---
+++ AbstractRTEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AbstractRTEx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("AbstractRTEx")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("250df744-d596-472a-845b-2b410d0a7bab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
